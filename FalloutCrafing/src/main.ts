a374b8a31fe388a19c17ee158c0b3daccb61ec06
Felix Langstengel <felix.langstengel@gmail.com>
2026-06-01 14:22:55 +0200

    add form field

--- FalloutCrafing/src/main.ts
+++ FalloutCrafing/src/main.ts
@@ -5,10 +5,31 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import AutoComplete from 'primevue/autocomplete';
+import FloatLabel from 'primevue/floatlabel';
+import YourItems from './views/YourItems.vue';
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+})
+
+app.component('AutoComplete', AutoComplete)
+app.component('FloatLabel', FloatLabel)
+app.component('YourItems', YourItems)
 
 app.mount('#app')
---
+++ FalloutCrafing/src/views/YourItems.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="card flex flex-wrap justify-center items-end gap-4" style="height: 100%;">
+        <FloatLabel>
+            <AutoComplete />
+        </FloatLabel>
+    </div>
+</template>
